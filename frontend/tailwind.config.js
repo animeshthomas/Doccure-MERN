de5a666d05182c6770d8499a5c27455b37142fd4
Animesh Thomas <animeshthomas262@gmail.com>
2023-12-28 17:45:55 +0530

    basic structure done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primaryColor: '#0067FF',
+        yellowColor: '#FEB60D',
+        purpleColor: '#9771FF',
+        irisBlueColor: '#01B5C5',
+        headingColor: '#181A1E',
+        textColor: '#4E545F',
+      },
+      boxShadow: {
+        panelShadow:"rgba(17,12,46,0.15) 0px 48px 100px 0px",
+      }
+    },
   },
   plugins: [],
 }
