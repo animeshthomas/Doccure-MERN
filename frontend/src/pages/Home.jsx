1fbbd3708bab7413c7752fbfc9918630c33bb341
Animesh Thomas <animeshthomas262@gmail.com>
2023-12-29 22:11:03 +0530

    services page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,8 @@ import icon02 from '../assets/images/icon02.png';
 import icon03 from '../assets/images/icon03.png';
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
+import About from '../components/About/About';
+import ServiceList from '../components/Services/ServiceList';
 const Home = () => {
   return (
     <>
@@ -119,7 +121,19 @@ const Home = () => {
           </div>
         </div>
       </section>
-  
+      <About />
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Medical Services</h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care. 
+            </p>
+          </div>
+          <ServiceList />
+        </div>
+      </section>
     </>
   );
 };
