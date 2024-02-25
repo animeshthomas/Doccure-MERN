d083133053475421128e3247f21bed30c6875b31
Animesh Thomas <animeshthomas262@gmail.com>
2024-02-25 14:12:37 +0530

    minor bug fixed in login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,29 +2,29 @@ import { useState, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../config'
 import { toast } from 'react-toastify'
-import {authContext} from '../context/AuthContext.jsx'
+import { authContext } from '../context/AuthContext.jsx'
 import HashLoader from 'react-spinners/HashLoader.js'
 
 
 const Login = () => {
 
   const [formData, setFormData] = useState({
-    email:'',
-    password:''
+    email: '',
+    password: ''
   })
 
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const {dispatch} = useContext(authContext)  
+  const { dispatch } = useContext(authContext)
 
   const handleInputChange = e => {
-    setFormData({... formData, [e.target.name]: e.target.value})
+    setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
   const submitHandler = async event => {
     event.preventDefault();
     setLoading(true);
-  
+
     try {
       const res = await fetch(`${BASE_URL}/auth/login`, {
         method: 'post',
@@ -33,13 +33,13 @@ const Login = () => {
         },
         body: JSON.stringify(formData)
       });
-  
+
       const result = await res.json();
-  
+
       if (!res.ok) {
         throw new Error(result.message);
       }
-  
+
       dispatch({
         type: 'LOGIN_SUCCESS',
         payload: {
@@ -48,26 +48,22 @@ const Login = () => {
           role: result.role,
         }
       });
-  
+
       console.log(result, "login data");
-  
+
       setLoading(false);
       toast.success(result.message);
-  
-      if (formData.email === "admin@gmail.com" && formData.password === "123") {
-        // Navigate to the admin dashboard
-        navigate('/admin-dashboard');
-      } else {
-        // Navigate to the user dashboard
-        navigate('/home');
-      }
-  
+
+      // Navigate to the user dashboard
+      navigate('/home');
+
+
     } catch (err) {
       toast.error(err.message);
       setLoading(false);
     }
   }
-  
+
 
   return (
     <section className="px-5 lg:px-0">
@@ -78,12 +74,12 @@ const Login = () => {
 
         <form className="py-4 md:py-0" onSubmit={submitHandler}>
           <div className="mb-5">
-            <input 
-              type="email" 
-              placeholder="Enter your Email" 
-              name="email" 
-              value={formData.email} 
-              onChange={handleInputChange} 
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
               className="w-full py-3 border-b border-solid border-[#0066ff61] 
               focus:outline-none focus:border-b-primaryColor text-[16px] leading-7
               text-headingColor placeholder:text-textColor cursor-pointer"
@@ -92,12 +88,12 @@ const Login = () => {
           </div>
 
           <div className="mb-5">
-            <input 
-              type="password" 
-              placeholder="Password" 
-              name="password" 
-              value={formData.password} 
-              onChange={handleInputChange} 
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
               className="w-full py-3 border-b border-solid border-[#0066ff61] 
               focus:outline-none focus:border-b-primaryColor text-[16px] leading-7
               text-headingColor placeholder:text-textColor cursor-pointer"
@@ -113,7 +109,7 @@ const Login = () => {
           </div>
 
           <p className="mt-5 text-textColor text-center">
-            Don&apos;t have an account? 
+            Don&apos;t have an account?
             <Link to='/register' className="text-primaryColor font-medium ml-1">
               Register
             </Link>
